4214fc68b2443ce2bc198aaf5ec607baad3225da
QQXing <QQXing@163.com>
2020-11-27 19:06:07 +0800

    java...

---
+++ gits/src/main/java/com/atguigu/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * @BelongsProject: gitTest
+ * @BelongsPackage: com.atguigu
+ * @Author: WYQ
+ * @CreateTime: 2020-11-27 16:50
+ * @param:
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("hello!world");
+    }
+}
